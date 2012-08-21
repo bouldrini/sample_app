01700713cee403dc7de5d88295c06a897be3bcfa
richard <richardwieditz@gmail.com>
2012-08-21 14:04:53 +0200

    finished first cut of the User model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,16 @@
 class UsersController < ApplicationController
-  # layout 'pages'
+   layout 'pages'
   
   def new
   end
+
+  def index
+    @user = User.all
+  end
+
+  def show
+    id = 3
+    @user = User.find(params[:id])
+  end
+
 end
