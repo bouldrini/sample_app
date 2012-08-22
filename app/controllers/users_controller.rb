9840ec9e587b9c710344594759d1021561ff9a36
richard <richardwieditz@gmail.com>
2012-08-22 15:20:47 +0200

    user sign-up added successfully

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,8 @@ class UsersController < ApplicationController
   layout 'pages'
   
   def new
+     @user = User.new
+    @title = "Sign up"
   end
 
   def index
@@ -11,6 +13,17 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @title = @user.name
+  end
+
+  def create
+    @user = User.new(params[:user]) 
+    if @user.save
+    flash[:success] = "Welcome to the Sample App!"
+    redirect_to @user  
+    else
+      @title = "Sign up"
+      render 'new'
+    end
   end
 
 end
